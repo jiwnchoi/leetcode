f6ff5368ab9f4ca33ab4bc53b5a34d55399bdea9
Jiwon Choi <jiwnchoi@gmail.com>
2025-06-07 21:54:05 +0900

    Time: 44 ms (26.74%) | Memory: 18.7 MB (24.92%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -1,16 +1,14 @@
 import heapq
+
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        reverse_nums = nums[::-1]
+        max_idx = 0
 
-        rechable = []
-        heapq.heappush(rechable, 0)
-
-        for i, num in enumerate(reverse_nums):
-            if i == 0:
-                continue
-            
-            if i - (-rechable[0]) <= num:
-                heapq.heappush(rechable, -i)
-
-        return True if rechable[0] == -len(nums)+1 else False
+        for i, num in enumerate(nums):
+            if i > max_idx:
+                return False
+            max_idx = max(max_idx, i + num)
+        
+        if max_idx >= len(nums)-1:
+            return True
+        return False
