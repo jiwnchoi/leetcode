d483350ce101ee2bc5635fcb0736d175fceb5b70
Jiwon Choi <jiwnchoi@gmail.com>
2025-06-10 10:54:51 +0900

    Time: 20 ms (65.07%) | Memory: 23.4 MB (18.89%) - LeetSync

--- 134-gas-station/gas-station.py
+++ 134-gas-station/gas-station.py
@@ -1,27 +1,18 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        sc = sum(cost)
-        sg = sum(gas)
-        if sc > sg:
-            return -1
-        
-        net = [
-            gas[i] - cost[i]
-            for i in range(len(gas))
-        ]
-
-        print(net)
-
         argmin_v = -1
         min_v = 1e5
 
         current = 0 
 
-        for i, v in enumerate(net):
-            current += v
+        for i, g in enumerate(gas):
+            current += g - cost[i]
             if current < min_v:
                 min_v = current
                 argmin_v = i
+
+        if current < 0:
+            return -1 
 
         return (argmin_v + 1) % len(gas)
         
