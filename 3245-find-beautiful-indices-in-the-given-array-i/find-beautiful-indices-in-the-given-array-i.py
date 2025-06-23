7ba79ea7757e5c4f31033c8accffe5c52f6722c9
Jiwon Choi <jiwnchoi@gmail.com>
2025-06-24 00:25:16 +0900

    Time: 135 ms (56.15%) | Memory: 22.3 MB (33.93%) - LeetSync

---
+++ 3245-find-beautiful-indices-in-the-given-array-i/find-beautiful-indices-in-the-given-array-i.py
@@ -0,0 +1,29 @@
+from bisect import bisect_left
+class Solution:
+    def beautifulIndices(self, s: str, a: str, b: str, k: int) -> List[int]:
+        a_candi = []
+        for i in range(len(s) - len(a)+1):
+            if s[i:i+len(a)] == a:
+                a_candi.append(i)
+
+        b_candi = []
+        for i in range(len(s) - len(b)+1):
+            if s[i:i+len(b)] == b:
+                b_candi.append(i)
+
+        result = []
+        
+        if len(a_candi) == 0 or len(b_candi) == 0:
+            return []
+
+        for i in a_candi:
+            x = bisect_left(b_candi, i)
+            print(x, len(a_candi), len(b_candi))
+            if x > 0 and abs(b_candi[x-1] - i) <= k:
+                result.append(i)
+            
+            elif x <= len(b_candi) -1 and abs(b_candi[x] - i) <= k:
+                result.append(i)
+
+        return result
+        
